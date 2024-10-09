9396c14598e9248a4577fa767d2b634500f11f2d
Kevin <dennisk.blas@gmail.com>
2024-10-08 22:46:19 -0500

    estructura, estilos y lógica responsive

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit, OnDestroy{
+  private blinkDestroy: any
+  private writeNameDestroy: any
 
+  ngAfterViewInit(): void {
+    this.blink()
+    this.writeLastName()
+  }
+  ngOnDestroy(): void {
+      if(this.blinkDestroy){
+        clearInterval(this.blinkDestroy)
+      }
+      if(this.writeNameDestroy){
+        clearInterval(this.writeNameDestroy)
+      }
+  }
+  active: boolean = true
+  blink(){
+    this.blinkDestroy = setInterval(()=>{
+      this.active = !this.active
+    }, 500)
+  }
+
+  nameOriginal: string = 'Blas';
+  nameArray: string[] = this.nameOriginal.split('')
+  showName: string = this.nameOriginal
+  writeLastName = () => {
+    this.writeNameDestroy = setInterval(()=>{
+      if(this.nameArray.length === 0){
+        this.showName = this.nameOriginal
+        this.nameArray = this.nameOriginal.split('')
+      } else{
+        this.nameArray.pop()
+        this.showName = this.nameArray.join('')
+      }
+    }, 300)
+  }
+
+  linkCV: string = 'https://drive.google.com/uc?id=1r4JIch_s6YbLsn7CP1k1FRVO5nzexq1J&export=download'
+  downLoadCV(){
+    const link = document.createElement('a'); 
+    link.href = this.linkCV;
+    link.click();
+  }
 }
