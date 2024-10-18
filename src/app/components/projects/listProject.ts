35a3e057f2f66e5d948e4a88a1f93b8a8d89671b
Kevin <dennisk.blas@gmail.com>
2024-10-18 12:25:08 -0500

    Lista de projectos actualizados con links de S3 AWS

--- src/app/components/projects/listProject.ts
+++ src/app/components/projects/listProject.ts
@@ -11,7 +11,7 @@ export interface Projects {
     description: string,
     images: Image,
     link?: string,
-    codeRepo: string,
+    codeRepo?: string,
     technology: string[]
 }
 
@@ -19,12 +19,12 @@ export const projects: Projects[] = [
     {
         idProject: 'game-of-thrones',
         title: 'Game Of Thrones',
-        imgOriginal: 'https://my-design-imitations.s3.us-east-2.amazonaws.com/design-03-Game-Of-Thrones/Design-Original.jpg',
-        description: 'imagen 1',
+        imgOriginal: 'https://my-design-imitations.s3.us-east-2.amazonaws.com/design-03-Game-Of-Thrones/light/Design-Original.jpg',
+        description: 'Este proyecto recrea una web inspirada en una imagen de Game of Thrones. Originalmente solo era para escritorio, pero lo he hecho responsive, añadiendo interacción y lógica con SCSS, TypeScript y Angular.',
         images: {
-            small: '',
-            medium: '',
-            large: ''
+            small: 'https://my-design-imitations.s3.us-east-2.amazonaws.com/design-03-Game-Of-Thrones/light/Mobile-Large.webp',
+            medium: 'https://my-design-imitations.s3.us-east-2.amazonaws.com/design-03-Game-Of-Thrones/light/Tablet-Large.webp',
+            large: 'https://my-design-imitations.s3.us-east-2.amazonaws.com/design-03-Game-Of-Thrones/light/Laptop-Standard.webp'
         },
         codeRepo: 'https://github.com/dkevlas/GameOfThonesClone.git',
         technology: ['HTML', 'SASS', 'TYPESCRIPT', 'ANGULAR'],
@@ -33,12 +33,12 @@ export const projects: Projects[] = [
     {
         idProject: 'your-name',
         title: 'Your Name',
-        imgOriginal: 'https://my-design-imitations.s3.us-east-2.amazonaws.com/design-01-Your-Name/Design-Original.jpg',
-        description: 'imagen 2',
+        imgOriginal: 'https://my-design-imitations.s3.us-east-2.amazonaws.com/design-01-Your-Name/light/Design-Original.webp',
+        description: 'Este proyecto mejora un diseño web encontrado en Pinterest. Lo adapté a un formato responsivo para celular, tablet y desktop usando Angular. Es modular, pero no incluye interacciones.',
         images: {
-            small: '',
-            medium: '',
-            large: ''
+            small: 'https://my-design-imitations.s3.us-east-2.amazonaws.com/design-01-Your-Name/light/Mobile-Large.webp',
+            medium: 'https://my-design-imitations.s3.us-east-2.amazonaws.com/design-01-Your-Name/light/Tablet-Large.webp',
+            large: 'https://my-design-imitations.s3.us-east-2.amazonaws.com/design-01-Your-Name/light/Laptop-Standard.webp'
         },
         codeRepo: 'https://github.com/dkevlas/YourNameClone.git',
         technology: ['HTML', 'SASS', 'ANGULAR']
@@ -46,12 +46,12 @@ export const projects: Projects[] = [
     {
         idProject: 'native',
         title: 'Native',
-        imgOriginal: 'https://my-design-imitations.s3.us-east-2.amazonaws.com/design-02-Native/Design-Original.png',
-        description: 'imagen 3',
+        imgOriginal: 'https://my-design-imitations.s3.us-east-2.amazonaws.com/design-02-Native/light/Design-Original.webp',
+        description: 'Este proyecto mejora un diseño web de Pinterest. Adapté el original, que solo era para una pantalla, a un formato responsivo para celular, tablet y desktop. Solo utilicé HTML y SCSS, sin interacción.',
         images: {
-            small: '',
-            medium: '',
-            large: ''
+            small: 'https://my-design-imitations.s3.us-east-2.amazonaws.com/design-02-Native/light/Mobile-Large.webp',
+            medium: 'https://my-design-imitations.s3.us-east-2.amazonaws.com/design-02-Native/light/Tablet-Large.webp',
+            large: 'https://my-design-imitations.s3.us-east-2.amazonaws.com/design-02-Native/light/Laptop-Standard.webp'
         },
         codeRepo: 'https://github.com/dkevlas/NativeClone.git',
         technology: ['HTML', 'SASS'],
